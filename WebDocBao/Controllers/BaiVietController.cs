74584d50bb85c659dc39135db5c072cfe633b2fc
dmmlmm456 <dmmlmm456@gmail.com>
2020-06-14 11:26:33 +0700

    Add project files.

---
+++ WebDocBao/Controllers/BaiVietController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebDocBao.Controllers
+{
+    public class BaiVietController : Controller
+    {
+        // GET: BaiViet
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
